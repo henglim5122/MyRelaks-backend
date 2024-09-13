e1d0b29df93118b3fc1815cc9a95170faf794132
kumararuben03 <kumararuben0306@gmail.com>
2024-09-13 11:55:09 +0800

    Ruben BG

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main>
+      <Appbar/>
       <router-view />
     </v-main>
   </v-app>
---
+++ frontend/src/components/Appbar.vue
@@ -0,0 +1,20 @@
+<template>
+                <v-container class="fill-height">
+                    <v-row>
+                        <v-col cols="3">
+                            <v-card>
+                                <v-img
+                                src="./../assets/images/images.jpeg">
+                            </v-img>
+                            </v-card>
+                        </v-col>
+                            
+                        <v-col cols="9">
+                            <h1>Kumara Ruben Kumaravelloo</h1>
+                            <p class="text-justify"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </v-col>
+                    </v-row>
+
+            </v-container>
+    
+</template>
