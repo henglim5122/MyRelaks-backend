8363c08c7a2c280c38d7dac5195c9940e7048f51
FutureDesign: Arts & Prints <admin@fdartsprints.com>
2024-10-28 20:12:15 +0800

    Add README.md and update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,25 +4,29 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base  
 
+# Load environment variables from .env file
 load_dotenv(dotenv_path='./../frontend/.env')
 
+# Get database credentials from environment variables
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-# print(f"User: {DB_USER}, Password: {DB_PASSWORD}, Host: {DB_HOST}, Port: {DB_PORT}, DB Name: {DB_NAME}")
+# Error handling when creating the engine
+try:
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, 
+                            pool_size=5,
+                            max_overflow=10,
+                            pool_timeout=30,
+                            pool_recycle=1800)
+except Exception as e:
+    print(f"Error creating engine: {e}")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, 
-                      pool_size=5,
-                      max_overflow=10,
-                      pool_timeout=30,
-                      pool_recycle=1800,
-                      )
-
+# Create a session local class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Create a declarative base class
 Base = declarative_base()
