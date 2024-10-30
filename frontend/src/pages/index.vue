e995d22aa3793c1f915299a43d4b2ccfcf3fb1ed
henglim5122 <henglim5122@gmail.com>
2024-10-30 14:41:28 +0800

    hi

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app>
-    <NavigationBar/>
+    <NavigationBar />
     <v-main>
+      <div>HI</div>
       <v-container max-width="1600px" id="container-wrapper">
-      <LandingCarousel :src="images" />
-      <div class="pa-5"></div>
-      <LandingDivider />
-      <div class="pa-5"></div>
-      <LandingFeature height="550px"/>
-      <div class="pa-5"></div>
-      <LandingDivider />
-      <div class="pa-5"></div>
-      <LandingMap />
-      <div class="pa-10"></div>
-      <FoodAndActivities />
-      <div class="pa-10"></div>
-    </v-container>
+        <LandingCarousel :src="images" />
+        <div class="pa-5"></div>
+        <LandingDivider />
+        <div class="pa-5"></div>
+        <LandingFeature height="550px" />
+        <div class="pa-5"></div>
+        <LandingDivider />
+        <div class="pa-5"></div>
+        <LandingMap />
+        <div class="pa-10"></div>
+        <FoodAndActivities />
+        <div class="pa-10"></div>
+      </v-container>
       <LandingChatBot height="600px" />
       <div class="pa-5"></div>
     </v-main>
@@ -31,12 +32,12 @@ export default {
   data() {
     return {
       images: [
-"src/assets/LandingCarousel/1.jpg",
-"src/assets/LandingCarousel/2.jpg",
-"src/assets/LandingCarousel/3.jpg",
-"src/assets/LandingCarousel/4.jpg",
-"src/assets/LandingCarousel/5.jpg",
-"src/assets/LandingCarousel/6.jpg",
+        "src/assets/LandingCarousel/1.jpg",
+        "src/assets/LandingCarousel/2.jpg",
+        "src/assets/LandingCarousel/3.jpg",
+        "src/assets/LandingCarousel/4.jpg",
+        "src/assets/LandingCarousel/5.jpg",
+        "src/assets/LandingCarousel/6.jpg",
       ],
     };
   },
